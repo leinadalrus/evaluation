2807e978ab9a0ea8e18d9ac4f98e46602c34b588
David <daniel.david.surla@gmail.com>
2023-12-11 11:21:00 +1100

    added phpunit workflow...
    ...apparently I had an unnecessary else-if statement. (This is why you don't do late night programming.)

--- foobar.php
+++ foobar.php
@@ -1,20 +1,22 @@
 <?php declare(strict_types=1);
 
-function foobar() {
+function foobar()
+{
   $foo = "foo";
   $bar = "bar";
-
-  $a_hundred = 100;
   $newlines = "\n";
 
-  for ($index = 0; $index <= sizeof($a_hundred); $index++) {
-    if ($index % 3 == 0) printf($foo.$newlines);
-    else if ($index % 5 == 0) printf($bar.$newlines);
-    else ($index % 3 != 0 || $index % 5 != 0) printf($foo.$bar.$newlines);
+  for ($index = 0; $index <= 100; $index++) {
+    if ($index % 3 == 0)
+      printf($foo . $newlines);
+    else if ($index % 5 == 0)
+      printf($bar . $newlines);
+    else
+      printf($foo . $bar . $newlines);
   }
 }
 
-function main() 
+function main()
 {
   foobar();
   return 0;
